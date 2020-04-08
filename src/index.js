8c89cd090e8126ee6ce5f4c6fd00921e627c5c2f
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-04-08 01:32:48 -0400

    Conditionally Rendering Content

--- src/index.js
+++ src/index.js
@@ -2,12 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	render() {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			lat: null,
+			long: null,
+			errorMessage: '',
+		};
+
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => console.log(position),
-			(error) => console.log(error)
+			(position) => {
+				this.setState({
+					lat: position.coords.latitude,
+					long: position.coords.longitude,
+				});
+			},
+			(error) => {
+				this.setState({
+					errorMessage: error.message,
+				});
+			}
 		);
-		return <div>Latitude: </div>;
+	}
+
+	render() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div> Latitude: {this.state.lat}</div>;
+		}
+
+		return <div>Loading</div>;
 	}
 }
 
