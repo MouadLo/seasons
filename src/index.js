38ae84fd895bc86be2b6a40c32509bad1373768c
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-04-08 04:24:39 -0400

    adding some styling

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SeasonDisplay from './SeasonDisplay';
+
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			lat: null,
-			long: null,
-			errorMessage: '',
-		};
-
+	state = {
+		lat: null,
+		long: null,
+		errorMessage: '',
+	};
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => {
+			(position) =>
 				this.setState({
 					lat: position.coords.latitude,
 					long: position.coords.longitude,
-				});
-			},
-			(error) => {
+				}),
+			(error) =>
 				this.setState({
 					errorMessage: error.message,
-				});
-			}
+				})
 		);
 	}
 
@@ -32,7 +29,7 @@ class App extends React.Component {
 		}
 
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div> Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
 		return <div>Loading</div>;
